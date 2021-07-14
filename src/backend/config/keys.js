2837e586debadd8876598880c86367ae4cc781a3
Himaja Chandaluri <67281829+HimajaChandaluri@users.noreply.github.com>
2021-07-14 13:44:39 -0700

    Update keys.js

--- src/backend/config/keys.js
+++ src/backend/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGOURI: process.env.PROD
     ? `mongodb://${process.env.DB_HOST}:27017`
-    : "mongodb+srv://sakethreddy:272password@cluster0.6ou4o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    : "mongodb+srv://url",
   JWT_SECRET: "abcdefg",
 };
